e8ad7cc0caa422a5b3694aa97407082bbd2190f3
OlgsCepcova <olga2kozlova@gmail.com>
2026-01-11 21:30:56 +0200

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PD1_Console_EF.Models;
+
+namespace PD1_Console_EF.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Student> Students => Set<Student>();
+    public DbSet<Course> Courses => Set<Course>();
+    public DbSet<Enrollment> Enrollments => Set<Enrollment>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=pd1_console_ef.db");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // lai nebūtu dubultie ieraksti vienam studentam tajā pašā kursā
+        modelBuilder.Entity<Enrollment>()
+            .HasIndex(e => new { e.StudentId, e.CourseId })
+            .IsUnique();
+    }
+}
+
---
+++ Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PD1_Console_EF.Models;
+
+public class Student
+{
+    public int Id { get; set; }
+
+    [Required, StringLength(80)]
+    public string FullName { get; set; } = "";
+
+    [Range(1, 120)]
+    public int Age { get; set; }
+
+    public List<Enrollment> Enrollments { get; set; } = new();
+}
+
